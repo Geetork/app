2faca5513d8f26c10a384752a7de1ee2e15c1cc9
Galina Li <galinaleespb@gmail.com>
2021-10-27 11:36:50 +0300

    add mongoose

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // modules setup
 const express = require('express');
+const mongoose = require('mongoose');
+const
+
 const app = express();
-
-
-
 // port setup
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  res.end();
-});
+async function start() {
+  try {
+    await mongoose.connect('', {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    });
+    // app.get('/', (req, res) => {
+    //   res.send('Hello World!');
+    //   res.end();
+    // });
+    app.listen(app.get('port'), () => {
+      console.log('App started on port', app.get('port'))
+    });
+  } catch (e) {
+    console.log(e);
+  };
+};
 
-app.listen(app.get('port'), () => {
-  console.log('App started on port', app.get('port'))
-});
-
+start();
